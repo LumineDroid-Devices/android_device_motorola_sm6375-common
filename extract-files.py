2b8ffa10f00870873c6cf57e2232b3b6e629cc38
dodyirawan85 <dev.irawans@gmail.com>
2025-09-21 20:08:28 +0700

    sm6375-common: Switch to prebuilt audio HAL
    Fixes no sound issue during voip calls when switch to speaker mode
    
    Change-Id: I71e1647f5ed2dc097a8a6008e1cb37d53f9549d5
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -62,6 +62,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     ('vendor/bin/init.kernel.post_boot-blair.sh', 'vendor/bin/init.kernel.post_boot-holi.sh'): blob_fixup()
         .regex_replace('ro.boot.using_zram_from_fstab', 'ro.vendor.zram.swapon'),
+    'vendor/lib/soundfx/libspeakerbundle.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
